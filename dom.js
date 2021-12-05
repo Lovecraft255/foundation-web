f148e8f5c0ecefb6fcd73e881285c8ec90c619ae
Lovecraft255 <dilanbemitez6@gmail.com>
2021-12-05 03:02:50 -0300

    Pagina de comunidad terminada, con las funciones de agregar y eliminar comentarios

---
+++ dom.js
@@ -0,0 +1,61 @@
+function createComent(){
+
+    let coment = document.getElementById("coment").value;
+    return coment;
+
+}
+
+function addComent( coment ){
+
+    const eliminateBtn = document.createElement("button");
+
+    eliminateBtn.classList.add("fa-trash-alt");
+
+    eliminateBtn.classList.add("fas");
+    
+    eliminateBtn.classList.add("button-size");
+
+    const comentDiv = document.createElement("div");
+
+    comentDiv.classList.add("bordes");
+
+    const descComent = document.createElement("p");
+
+    descComent.textContent = coment;
+
+    comentDiv.append( descComent );
+
+    comentDiv.append(eliminateBtn);
+  
+    comentsContent.append(comentDiv);
+
+}
+
+let comentsContent = document.getElementById("coments-content");
+
+let btnPost = document.getElementById("btn-public");
+
+comentsContent.addEventListener( "click" , (e) => {
+
+    if( e.target.tagName == "BUTTON" ){
+
+        console.log( e.target.parentElement );
+
+        const parent = e.target.parentElement;
+
+        parent.remove();
+
+        alert( "Se eliminaea el comentario" );
+
+    } 
+})
+
+btnPost.addEventListener( "click" , () => {
+
+    const coment = createComent();
+
+    console.log(coment);
+
+    addComent( coment );
+
+} );
